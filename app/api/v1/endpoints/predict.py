b94d78866f98b2b7400675c8448c5acb3b0762d1
Ankur Biswas <biswasankur502@gmail.com>
2026-05-05 20:42:20 +0530

    fix: add uvicorn for deployment

--- app/api/v1/endpoints/predict.py
+++ app/api/v1/endpoints/predict.py
@@ -1,36 +1,10 @@
-import os
-import joblib
+from fastapi import APIRouter
+from app.application.pipeline import run_pipeline
+from app.schemas.request import PredictRequest
 
-model = None
+router = APIRouter()
 
-def load_model():
-    global model
-
-    if model is None:
-        try:
-            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-            MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
-
-            model = joblib.load(MODEL_PATH)
-            print("✅ Model loaded")
-
-        except Exception as e:
-            print("❌ Model load failed:", str(e))
-            model = None
-
-
-def predict_irradiance(data):
-    load_model()
-
-    if model is None:
-        return 50  # fallback
-
-    try:
-        prediction = model.predict([
-            [data["temp"], data["cloud"], data["humidity"]]
-        ])
-        return float(prediction[0])
-
-    except Exception as e:
-        print("Prediction error:", str(e))
-        return 50
+@router.post("/predict")
+async def predict(data: PredictRequest):
+    result = await run_pipeline(data)
+    return result
